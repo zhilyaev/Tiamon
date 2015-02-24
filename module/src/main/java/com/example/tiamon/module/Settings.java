01190f416038228d88584bedf059b4bf8652f03f
zhilyaev <dmitriy@zhilyaev.net>
2015-02-24 12:06:00 +0300

    Добавлены функции по умолчанию

--- module/src/main/java/com/example/tiamon/module/Settings.java
+++ module/src/main/java/com/example/tiamon/module/Settings.java
@@ -1,7 +1,40 @@
 package com.example.tiamon.module;
 
-/**
- * Created by Diamon on 23.02.2015.
- */
-public class Settings {
+
+import java.io.*;
+
+public class Settings{
+    public String path = "pet.out";
+
+    /* Получить настйроки питомца из файла */
+    Pet GetPet(String path) throws IOException, ClassNotFoundException {
+        FileInputStream fis = new FileInputStream(path);
+        ObjectInputStream oin = new ObjectInputStream(fis);
+        return (Pet) oin.readObject();
+    }
+
+    Pet GetPet() throws IOException, ClassNotFoundException {
+        FileInputStream fis = new FileInputStream(this.path);
+        ObjectInputStream oin = new ObjectInputStream(fis);
+        return (Pet) oin.readObject();
+    }
+
+    /* Сохрнаить питомца в файл */
+    void SavePet(Pet pet, String path) throws IOException{
+        FileOutputStream fos = new FileOutputStream(path);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(pet);
+        oos.flush();
+        oos.close();
+    }
+
+    void SavePet(Pet pet) throws IOException{
+        FileOutputStream fos = new FileOutputStream(this.path);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(pet);
+        oos.flush();
+        oos.close();
+    }
+
+
 }
