7e4633f84d69d70fe06f07f59e613d436865055f
zhilyaev <dmitriy@zhilyaev.net>
2015-03-14 16:21:12 +0300

    Return game 2

--- module/src/main/java/ru/diamon/tiamon/Game_Zone.java
+++ module/src/main/java/ru/diamon/tiamon/Game_Zone.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 import java.util.Date;
 import java.util.Random;
 
+import static java.lang.Thread.sleep;
+
 public class Game_Zone extends Index {
     /* Найстройки: Статусы [INT] ∈ [0;100] */
     protected final String _STATUS_HANGRY = "HANGRY";
@@ -26,8 +28,10 @@ protected final String _SHOP_FISH = "FISH";*/
 
     protected TextView TV_age, TV_name;
     protected Intent intent_shop;
-    //protected static Thread T,events;
+    protected static Thread T,events;
     protected static Random random;
+    protected final int U = 3000;
+    protected final int FIRST_TIME = 1000*60*60*12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +47,58 @@ protected final String _SHOP_FISH = "FISH";*/
 
     protected void live(){
 // Муахаха ЛЯМБДА
+        T = new Thread((Runnable) () -> {
+            while (true) {
+                try {
+                    sleep(PET.getLong(_NEXTTIME, FIRST_TIME));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+/* Изменить СТАТУСЫ */
+                changeStatus(_STATUS_SLEEP, -random.nextInt(15));
+                changeStatus(_STATUS_HANGRY, -random.nextInt(14));
+                changeStatus(_STATUS_PLAY, -random.nextInt(13));
+
+// Умри
+                if (isDie()) break;
+
+/* Время на Следущий заход */
+                E = PET.edit();
+// NEXTTIME = Сейчас + Время разрыва
+                E.putLong(_NEXTTIME, new Date().getTime() + PET.getLong(_TIME, FIRST_TIME) - U);
+                E.putLong(_TIME, PET.getLong(_TIME, FIRST_TIME) - U);
+// AGE = (Сейчас-Родился) / 24 часа = {дни}
+                E.putLong(_AGE, (new Date().getTime() - PET.getLong(_BURN, 0)) % (FIRST_TIME * 2));
+                E.apply();
+
+            }// Цикл
+        });
+
+        events = new Thread((Runnable) () ->
+        {
+            while (true) {
+                try {
+                    sleep(U * 2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                if (random.nextInt(777) + 1 == 777) {
+                    informer(String.valueOf(R.string.lottery));
+                    E = PET.edit();
+                    E.putLong(_MONEY, PET.getInt(_MONEY, 0) + 777);
+                    E.apply();
+                }
+
+// Умри
+                if (isDie()) break;
+            }// Цикл
+        });
+
+/* Я сказала, СТАРТУЕМ! */
+        T.start();
+        events.start();
     }
 
     @Override
