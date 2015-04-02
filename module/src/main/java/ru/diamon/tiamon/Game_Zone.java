9b86808ee7560542224fb771df813e56a3d6569e
zhilyaev <dmitriy@zhilyaev.net>
2015-04-02 18:04:11 +0300

    Tiamon VERSION 5

--- module/src/main/java/ru/diamon/tiamon/Game_Zone.java
+++ module/src/main/java/ru/diamon/tiamon/Game_Zone.java
@@ -2,6 +2,7 @@ package ru.diamon.tiamon;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.ImageButton;
@@ -14,12 +15,15 @@ import java.util.Random;
 
 public class Game_Zone extends Index {
 
-    TextView TV_age, TV_name,TV_catbucks;
+    TextView TV_age, TV_name,TV_catbucks,tv_sleep,tv_play,tv_hangry;
     ImageButton btn_shop;
     ImageView ripView;
     Intent intent_shop;
-    Thread live;
+    Handler head_sleep,head_play,head_hangry,h;
+    ProgressBar bar_sleep,bar_play,bar_hangry;
     WebView petView;
+    Thread life;
+    boolean die = false;
 
     static Random random;
     final int U = 3000;
@@ -32,7 +36,6 @@ public class Game_Zone extends Index {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         random = new Random();
-        loadPet();
 
         petView = (WebView) findViewById(R.id.PetView);
         TV_name = (TextView) findViewById(R.id.label_name);
@@ -40,12 +43,48 @@ public class Game_Zone extends Index {
         TV_catbucks = (TextView) findViewById(R.id.catbucks);
         btn_shop = (ImageButton) findViewById(R.id.btn_shop);
         ripView = (ImageView) findViewById(R.id.rip);
+        tv_sleep = (TextView) findViewById(R.id.TV_sleep);
+        tv_play = (TextView) findViewById(R.id.TV_play);
+        tv_hangry = (TextView) findViewById(R.id.TV_hangry);
+        bar_sleep = (ProgressBar) findViewById(R.id.bar_sleep);
+        bar_play = (ProgressBar) findViewById(R.id.bar_play);
+        bar_hangry = (ProgressBar) findViewById(R.id.bar_hangry);
 
+        head_sleep = new Handler() {
+            public void handleMessage(android.os.Message msg) {
+                tv_sleep.setText(String.valueOf(msg.what));
+            }
+        };
+        head_play = new Handler() {
+            public void handleMessage(android.os.Message msg) {
+                tv_play.setText(String.valueOf(msg.what));
+            }
+        };
+        head_hangry = new Handler() {
+            public void handleMessage(android.os.Message msg) {
+                tv_hangry.setText(String.valueOf(msg.what));
+            }
+        };
         btn_shop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(intent_shop);
             }
+        });
+
+        life = new Thread((Runnable) ()->{
+
+            do {
+                try {Thread.sleep(1000);}
+                catch (InterruptedException e) {System.out.println("thread error");}
+
+                _status_PLAY -= random.nextInt(10);
+                _status_HANGRY -= random.nextInt(10);
+                _status_SLEEP -= random.nextInt(10);
+
+                upStatus();
+
+            } while (!die);
         });
     }
 
@@ -69,11 +108,8 @@ public class Game_Zone extends Index {
         TV_age.setText(String.valueOf(_AGE));
         TV_catbucks.setText(String.valueOf(_MONEY));
 
-        new Thread((Runnable) ()-> {
-            while(true){
+        life.start();
 
-            }
-        }).start();
     }
 
     @Override
@@ -85,18 +121,25 @@ public class Game_Zone extends Index {
     }
 
     public void upStatus(){
-        ProgressBar sleep = (ProgressBar) findViewById(R.id.status_sleep);
-        ProgressBar play = (ProgressBar) findViewById(R.id.status_play);
-        ProgressBar hangry = (ProgressBar) findViewById(R.id.status_hangry);
 
-        sleep.setProgress(_status_SLEEP);
-        play.setProgress(_status_PLAY);
-        hangry.setProgress(_status_HANGRY);
+        if(_status_HANGRY<0){_status_HANGRY=0;}
+        if(_status_SLEEP<0){_status_SLEEP=0;}
+        if(_status_PLAY<0){_status_PLAY=0;}
+
+        if(_status_HANGRY>100){_status_HANGRY=100;}
+        if(_status_SLEEP>100){_status_SLEEP=100;}
+        if(_status_PLAY>100){_status_PLAY=100;}
+
+        head_hangry.sendEmptyMessage(_status_HANGRY);
+        head_sleep.sendEmptyMessage(_status_SLEEP);
+        head_play.sendEmptyMessage(_status_PLAY);
+
+        bar_sleep.setProgress(_status_SLEEP);
+        bar_play.setProgress(_status_PLAY);
+        bar_hangry.setProgress(_status_HANGRY);
 
         if(_status_HANGRY<=0 ||_status_SLEEP<=0 ||_status_PLAY<=0){
-            petView.setEnabled(false);
-            btn_shop.setEnabled(false);
-            ripView.setVisibility(View.VISIBLE);
+            die=true;
         }
     }
 }
